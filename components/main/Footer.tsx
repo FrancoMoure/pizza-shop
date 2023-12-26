cdd5e9f19381c3cbd6d9935e9b0647f542c17090
Franco Moure <francomoureitschat@gmail.com>
2023-12-26 08:17:17 -0300

    installing react icons

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -5,6 +5,7 @@ import{
 
 import { FaYoutube } from 'react-icons/fa'
 
+
 const Footer = () => {
     return(
         <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]">
